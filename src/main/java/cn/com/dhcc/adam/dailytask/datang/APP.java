2fdd00edecc382f012da3815058310a953f74a5a
cedoo <cedoo@qq.com>
2015-12-11 12:13:25 +0800

    remove no need

--- src/main/java/cn/com/dhcc/adam/dailytask/datang/APP.java
+++
@@ -1,9 +0,0 @@
-package cn.com.dhcc.adam.dailytask.datang;
-
-import java.util.Arrays;
-
-public class APP {
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString("ass".split("_")));
-	}
-}
